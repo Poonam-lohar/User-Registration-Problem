2f148f630e023fe1eb674021334264c6ea128121
Your Name <you@example.com>
2022-12-01 13:23:14 +0530

    Added: user need to enter valid first name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,9 +1,26 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome To User Registration Problem....");
+    public boolean validFirstName() {
+
+        System.out.println("Enter First Name: ");
+        String name = sc.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        boolean result = matcher.matches();
+
+        if (result)
+            System.out.println("First name is valid..");
+        else
+            System.out.println("First name is invalid..");
+        return result;
     }
 }
